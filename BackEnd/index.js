b18d4fe068d6b5fbaefcc600cff3b338dbc87067
deepak yadav <deepak.yadav@identixweb.in>
2025-12-27 23:10:38 +0530

    Reapply "removed db function from function which is commented"
    This reverts commit ce251e9af91fe7619de002efc8658bb7d8c0a45d.

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,6 +9,7 @@ const { handleError } = require('./src/handle_response');
 dotenv.config();
 const app = express();
 
+//middlewares
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
@@ -17,16 +18,13 @@ app.use(cors({
 }))
 app.use(cookieParser())
 // app.use('/', verifyToken)
-// const dbConnection = (req, res) => {
 console.log('from db coneection')
 mongoose.connect(process.env.MONGODB_URI)
     .then(working => console.log('data base connected successfully'))
     .catch(error => 'something wrong to connection with db');
-// }
 
 
 app.use(express.json());
-// app.use(dbConnection)
 app.use('/api', router);
 
 app.listen(process.env.PORT, () => console.log(`server is running on PORT ${process.env.PORT}`));
